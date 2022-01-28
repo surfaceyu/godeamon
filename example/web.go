79aaaeffffd8474fc78076d382c736d05c1bfefd
chenhongyu <surfaceyu@outlook.com>
2022-01-28 17:00:09 +0800

    support use flag in your apps

--- example/web.go
+++ example/web.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
-	_ "github.com/surfaceyu/godeamon"
+	flag "github.com/spf13/pflag"
+	godeamon "github.com/surfaceyu/godeamon"
 )
 
 func main() {
+	goDaemonF := flag.Bool("f", false, "run app with -f.")
+
+	godeamon.App.Run()
+	fmt.Println("=============>", *goDaemonF)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/index", func(rw http.ResponseWriter, req *http.Request) {
 		rw.Write([]byte("hello, golang!\n"))
--- godeamon.go
+++ godeamon.go
@@ -8,11 +8,22 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
+type DeamonApp struct {
+	deamon *bool
+}
+
+var App *DeamonApp
+
 func init() {
 	goDaemon := flag.BoolP("deamon", "d", false, "run app as a daemon with -d.")
-	flag.Parse()
+	App = &DeamonApp{
+		deamon: goDaemon,
+	}
+}
 
-	if *goDaemon {
+func (app *DeamonApp) Run() *DeamonApp {
+	flag.Parse()
+	if *app.deamon {
 		cmd := exec.Command(os.Args[0], flag.Args()...)
 		if err := cmd.Start(); err != nil {
 			fmt.Printf("start %s failed, error: %v\n", os.Args[0], err)
@@ -21,4 +32,5 @@ func init() {
 		fmt.Printf("%s [PID] %d running...\n", os.Args[0], cmd.Process.Pid)
 		os.Exit(0)
 	}
+	return app
 }
